5916b25a032dbb054b9a39683217b3c995159110
= <tracchiabelen@gmail.com>
2022-04-24 17:01:46 -0300

    Clase rdp

--- rdp.c
+++ rdp.c
@@ -5,9 +5,22 @@ void printNumero(rdp_o * rdp){
     printf("Numero: %d \n",rdp->numero);
 }
 
+int isPos (rdp_o * rdp,int index){
+    return 0;
+}
+
+
 struct rdp_metodos rdpMetodos ={
 
-    .printNumero = printNumero
+    .printNumero = printNumero,
+    .isPos = isPos,
+    // .updateTimeStamps = updateTimeStamps,
+    // .getMarkVector = getMarkVector,
+    // .getUseBuffer = getUseBuffer,
+    // .getIsBuffer = getIsBuffer,
+    // .getCountBuffer = getCountBuffer,
+    // .getSensitized = getSensitized,
+    // .ifEnd = ifEnd
 };
 
 extern void new_rdp(rdp_o * p_rdp)
@@ -15,3 +28,5 @@ extern void new_rdp(rdp_o * p_rdp)
    p_rdp->numero = 2;
    p_rdp->metodos = &rdpMetodos;
 }
+
+
--- rdp.h
+++ rdp.h
@@ -2,14 +2,49 @@ typedef struct rdp rdp_o;
 
 
 struct rdp_metodos{
+
     void (*printNumero) (rdp_o *);
+    int  (*isPos) (rdp_o *,int index);
+    void (*updateTimeStamps) (rdp_o *, int * oldSens);
+    int * (* getMarkVector)(rdp_o *);
+    int * (*getUseBuffer)(rdp_o *);
+    int * (*getIsBuffer)(rdp_o *);
+    int * (*getCountBuffer)(rdp_o *);
+    int * (*getSensitized)(rdp_o *);
+    int   (*ifEnd)(rdp_o *);
 };
 
 struct rdp{
+   
+    int numero; //Para probar
 
-    int numero;
+    //Cualquier tamaño, despues hay que cambiarlo
+    int M[2];
+    int B[2];
+    int I[2][2];
+    int Ipos[2][2];
+    int Ineg[2][2];
+    int H[2][2];
+    int E[2];
+
+    int sensitivedTime[2]; //Cambiar, es de tipo LocalTime. No se como manejar fechas aca
+    int minTimeArrival;
+    int minTimeSrv1;
+    int minTimesSrv2;
+
+    int useBuffers[4];
+    int isBuffer[2];
+    int isGenTransition[1];
+    int isAddBuffer[2];
+
+    int estados;
+    int transiciones;
+    int dataNumber;
+    int packetCounter; //Es boolean
+
     const struct rdp_metodos * metodos;
 };
 
 //Funciones
+
 void new_rdp(rdp_o * p_rdp);
