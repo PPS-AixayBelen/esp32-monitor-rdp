db01486cd4fff1baf68ac262e049636e33f8dcc1
AixaCarrizo <craftech.aixa>
2022-05-01 20:19:22 -0300

    eliminadas funciones inncesesarias de la clase monitor

--- monitor.c
+++ monitor.c
@@ -6,31 +6,15 @@ void printNumero(monitor_o * monitor){
 }
 
 
-void initConditions (monitor_o * monitor){ //podriamos no poner esta funcion, si solo inicializa a 0 todo el array
-
-    for (int i = 0; i > monitor->numberTransitions; i++){
-        monitor->boolQuesWait[i] = 0;
-        // quesWait.add (lock.newCondition ());
-
-    }
-
-    // politica = new Politica (pn);
-
-}
-
-
-char *getTransitions (monitor_o *monitor){ //creo que esta funcion la usabamos para el log, ya no seria necesaria
-        return monitor->transitions;
-}
-
 
 int verifyMInvariants (monitor_o *monitor){
 
     // int *mark = pn.getMarkVector();
+    int mark [15];
     if (((mark[1] + mark[7]) == 1) && ((mark[4] + mark[12] + mark[14]) == 1) && ((mark[5] + mark[13] + mark[15]) == 1) && ((mark[0] + mark[6]) == 1) && ((mark[8] + mark[9]) == 1))
         return 0;
     else 
-        return 1; //rompiose
+        return 1; //rompiose        
 
 }
 
@@ -65,21 +49,6 @@ void finalSignalPolitic (monitor_o *monitor){
 
 }
 
-
-void showBoolQueuesWait (monitor_o *monitor){ //funcion para debug, tampoco es necesaria
-
-
-    printf("Dummy print \n");
-
-    // int aux[] = pn.getSensitized ();
-    // int count = 0;
-    //     for (boolean item : boolQuesWait) {
-    //         if (printDebug) System.out.println ("index:" + count + "    " + item + "  ---   " + aux[count]);
-    //         count++;
-    //     }
-    //     if (printDebug)
-    //         System.out.println ("--------------------------------- 0 --------------------------------------------");
-}
 
 
 int shoot (monitor_o *monitor, int index){
@@ -133,23 +102,20 @@ int shoot (monitor_o *monitor, int index){
 struct monitor_metodos monitorMetodos ={
 
     .printNumero = printNumero,
-    .initConditions = initConditions,
-    .getTransitions = getTransitions,
     .verifyMInvariants = verifyMInvariants,
     .signalPolitic = signalPolitic,
     .finalSignalPolitic = finalSignalPolitic,
-    .showBoolQueuesWait = showBoolQueuesWait,
     .shoot = shoot
 };
 
-extern void new_monitor(monitor_o * p_monitor)
+extern void new_monitor(monitor_o * p_monitor, pthread_mutex_t mutex, pthread_cond_t *espera, int numberTransitions, int *boolQuesWait)
 {
     p_monitor->numero = 2;
-    p_monitor->numberTransitions = 15;
-    int boolQuesWait[p_monitor->numberTransitions];
+    p_monitor->mutex = mutex;
+    p_monitor->espera = espera;
+    p_monitor->numberTransitions = numberTransitions;
     p_monitor->boolQuesWait = boolQuesWait;
     p_monitor->end = 0;
-    p_monitor->transitions = "";
     p_monitor->metodos = &monitorMetodos;
 }
 
--- monitor.h
+++ monitor.h
@@ -11,32 +11,26 @@ typedef struct monitor monitor_o;
 struct monitor_metodos{
 
     void (*printNumero) (monitor_o *);
-    void (*initConditions) (monitor_o *);
-    char *(*getTransitions) (monitor_o *); //creo que este metodo era para los logs asi que capaz no haria falta
     int (*verifyMInvariants)(monitor_o *);
     void (*signalPolitic)(monitor_o *);
     void (*finalSignalPolitic)(monitor_o *);
-    void (*showBoolQueuesWait)(monitor_o *);
     int (*shoot)(monitor_o *, int index);
 };
 
 struct monitor{
     
     int numero; //Para probar
-
+    
     int numberTransitions;
-    //revisar como se asocian los mutex a las condiciones (si un mutex se puede asociar a mas de una condicion, por ej)
     pthread_mutex_t mutex; //reemplaza al reentrant lock
     pthread_cond_t *espera; //reemplazaria al quesWait, tendria que haber una por cada transcion
     int *boolQuesWait; //0 = no esta esperando , 1 = esta esperando
-
     // Politica politica;
     // PN pn;
     int end;
-    char *transitions;
-
-    const struct monitor_metodos * metodos;
+    const struct monitor_metodos * metodos;    
 
 };
 
-void new_monitor(monitor_o * p_monitor);
+
+void new_monitor(monitor_o *p_monitor, pthread_mutex_t mutex,pthread_cond_t *espera, int numberTransitions, int *boolQuesWait);
