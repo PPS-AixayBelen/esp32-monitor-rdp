42e6d2c95312b5cffbd759ba91789f803eae95b3
AixaCarrizo <39010117+AixaCarrizo@users.noreply.github.com>
2022-05-16 20:39:14 -0300

    Feat: Tests implementados  (#6)
    * Test de invariantes de plaza
    
    * Movi los log al monitor
    
    * Test de invariantes de transicion
    
    * Se modificó el logueo de los disparos, ahora el archivo se escribe desde main.c
    
    Co-authored-by: = <tracchiabelen@gmail.com>

--- cpuProcess.c
+++ cpuProcess.c
@@ -2,45 +2,45 @@
 #include <stdio.h>
 #include <unistd.h>
 
-
-
-void *run(cpuProcess_o *cpuProcess){
+void *run(cpuProcess_o *cpuProcess)
+{
     printf("VOY A DISPARAR: %d\n", cpuProcess->processList[0]);
     sleep(3);
     int shootResult = 0;
-    while(1){
-        for (int i = 0; i < (cpuProcess->processNum); i++){
+    while (1)
+    {
+        for (int i = 0; i < (cpuProcess->processNum); i++)
+        {
             printf("Nro de transicion a disparar: %d\n", cpuProcess->processList[i]);
             shootResult = cpuProcess->monitor->metodos->shoot(cpuProcess->monitor, cpuProcess->processList[i]);
             printf("shootResult - %d disparo- %d\n", shootResult, cpuProcess->processList[i]);
             // shootResult = monitor.shoot(cpuProcess->processList[i]);
-            if(shootResult == -1){
+            if (shootResult == -1)
+            {
                 printf("END: %d", i);
-                return NULL;           
+                return NULL;
             }
-            else if (shootResult > 0){
+            else if (shootResult > 0)
+            {
                 printf("me falta tiempo para sensibilizarme\n");
-                sleep(shootResult + 1 );
+                sleep(shootResult + 1);
                 cpuProcess->monitor->metodos->shoot(cpuProcess->monitor, cpuProcess->processList[i]);
                 // monitor.shoot(cpuProcess->processList[i]);
             }
-            else{
+            else
+            {
             }
         }
     }
 }
 
-struct cpuProcess_metodos cpuMetodos ={
-    .run = run
-};
+struct cpuProcess_metodos cpuMetodos = {
+    .run = run};
 
-extern void new_cpuProcess(cpuProcess_o * p_cpuProcess, int *processList, int processNum, monitor_o *monitor)
+extern void new_cpuProcess(cpuProcess_o *p_cpuProcess, int *processList, int processNum, monitor_o *monitor)
 {
     p_cpuProcess->monitor = monitor;
     p_cpuProcess->processNum = processNum;
     p_cpuProcess->metodos = &cpuMetodos;
     p_cpuProcess->processList = processList;
-
 }
-
-
--- main.c
+++ main.c
@@ -1,5 +1,5 @@
-//Defining _REENTRANT causes the compiler to use thread safe (i.e. re-entrant) 
-//versions of several functions in the C library.
+// Defining _REENTRANT causes the compiler to use thread safe (i.e. re-entrant)
+// versions of several functions in the C library.
 #define _REENTRANT
 
 // #include "monitor.h"
@@ -7,20 +7,18 @@
 #include <string.h>
 #include "leerMatriz.h"
 
-int main(){
-
+int main()
+{
+    
     pthread_attr_t atrib;
     pthread_t c[15];
 
     pthread_attr_init(&atrib);
     pthread_attr_setscope(&atrib, PTHREAD_SCOPE_SYSTEM);
-
-
     pthread_attr_setschedpolicy(&atrib, SCHED_OTHER);
+    
     monitor_o mymonitor;
     rdp_o rdp;
-    pthread_attr_setscope(&atrib, PTHREAD_SCOPE_SYSTEM);
-
     monitor_o *monitor = &mymonitor;
     cpuProcess_o tShooter[15];
     cpuProcess_o t0Shooter;
@@ -28,63 +26,64 @@ int main(){
     cpuProcess_o t8Shooter;
 
     int *shootArray[15];
-    int shootSequence1[1]={0}; //dispara T0 
-    int shootSequence2[1]={5}; //dispara T1
-    int shootSequence3[1]={13}; //dispara T8
-    int shootSequence4[1]={11}; //dispara T6 (Power UP CPU1)
-    int shootSequence5[1]={12}; //dispara T7 (CPU ON 1)
-    int shootSequence6[1]={7}; // T13 POWER UP 2
-    int shootSequence7[1]={6}; //T12
-    int shootSequence8[1]={9}; //T2
-    int shootSequence9[1]={3}; //Service rate 1
-    int shootSequence10[1]={1};  //Power down 1
-    int shootSequence11[1]={2}; //Power down 2
-    int shootSequence12[1]={14}; //T9
-    int shootSequence13[1]={4}; //Service rate 2
-    int shootSequence14[1]={8};  //T14
-    int shootSequence15[1]={10};  //T5
+    int shootSequence1[1] = {0};   // dispara T0
+    int shootSequence2[1] = {5};   // dispara T1
+    int shootSequence3[1] = {13};  // dispara T8
+    int shootSequence4[1] = {11};  // dispara T6 (Power UP CPU1)
+    int shootSequence5[1] = {12};  // dispara T7 (CPU ON 1)
+    int shootSequence6[1] = {7};   // T13 POWER UP 2
+    int shootSequence7[1] = {6};   // T12
+    int shootSequence8[1] = {9};   // T2
+    int shootSequence9[1] = {3};   // Service rate 1
+    int shootSequence10[1] = {1};  // Power down 1
+    int shootSequence11[1] = {2};  // Power down 2
+    int shootSequence12[1] = {14}; // T9
+    int shootSequence13[1] = {4};  // Service rate 2
+    int shootSequence14[1] = {8};  // T14
+    int shootSequence15[1] = {10}; // T5
 
-    shootArray [0] = shootSequence1;
-    shootArray [1] = shootSequence2;
-    shootArray [2] = shootSequence3;
+    shootArray[0] = shootSequence1;
+    shootArray[1] = shootSequence2;
+    shootArray[2] = shootSequence3;
     shootArray[3] = shootSequence4;
     shootArray[4] = shootSequence5;
-    shootArray [5] = shootSequence6;
-    shootArray [6] = shootSequence7;
-    shootArray [7] = shootSequence8;
+    shootArray[5] = shootSequence6;
+    shootArray[6] = shootSequence7;
+    shootArray[7] = shootSequence8;
     shootArray[8] = shootSequence9;
     shootArray[9] = shootSequence10;
-    shootArray [10] = shootSequence11;
-    shootArray [11] = shootSequence12;
+    shootArray[10] = shootSequence11;
+    shootArray[11] = shootSequence12;
     shootArray[12] = shootSequence13;
     shootArray[13] = shootSequence14;
     shootArray[14] = shootSequence15;
-     
-    pthread_mutex_t mutex; 
+
+    pthread_mutex_t mutex;
     pthread_cond_t espera[15];
     int boolQuesWait[15] = {0};
 
     pthread_mutex_init(&mutex, NULL);
-    for (int i = 0; i < 15; i++){
+    for (int i = 0; i < 15; i++)
+    {
         pthread_cond_init(&espera[i], NULL);
     }
     new_rdp(&rdp);
     new_monitor(&mymonitor, mutex, espera, 15, boolQuesWait, &rdp);
 
-    for ( int i = 0;i<15;i++)
+    for (int i = 0; i < 15; i++)
     {
-        new_cpuProcess(&tShooter[i],shootArray[i], 1, monitor);
+        new_cpuProcess(&tShooter[i], shootArray[i], 1, monitor);
     }
 
-    // monitor->metodos->shoot(monitor, 0);
-    // monitor->metodos->shoot(monitor, 5);
     for (int i = 0; i < 15; i++)
-        pthread_create(&c[i], &atrib,(&tShooter[i])->metodos->run , (cpuProcess_o *) &tShooter[i]);
+        pthread_create(&c[i], &atrib, (&tShooter[i])->metodos->run, (cpuProcess_o *)&tShooter[i]);
 
-    for (int i = 0; i < 15; i++){
+    for (int i = 0; i < 15; i++)
+    {
         pthread_join(c[i], NULL);
     }
+    FILE * invTransicionFile = fopen("./test/InvarianteTransicion","w+");
+    fputs(monitor->logInvTransicion,invTransicionFile);
+    fclose(invTransicionFile);
     printf("\nTERMINEEEEEE\n");
-
 }
-
